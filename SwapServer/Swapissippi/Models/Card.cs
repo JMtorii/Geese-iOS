92279bb5d28015ee4d9eae3188a2d326dc671393
EvanC <evan.crothers@gmail.com>
2015-02-27 20:59:59 -0500

    Base server implementation

---
+++ SwapServer/Swapissippi/Models/Card.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Swapissippi.Models
+{
+    public class Card
+    {
+        public long SenderId { get; set; }
+        public string Content { get; set; }
+    }
+}
